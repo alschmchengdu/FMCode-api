96630f7d4ecf63b5dc610605d6c8058916ad14c0
Alison Schmidt <alison.e.schmidt@gmail.com>
2018-05-12 16:47:16 +0800

    view now shows json

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+15.times do
+  Item.create(name: "#{Faker::Commerce.color}",content: "#{Faker::Company.buzzword} #{Faker::Food.ingredient}".capitalize)
+end
+
